e3dbed3577e5d3e6cac9d42f5069330440fde538
AlexCampos98 <acamposm02@educantabria.com>
2021-01-26 18:35:36 +0100

    Clase de prueba

---
+++ src/pruebas/NewClass.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebas;
+
+/**
+ *
+ * @author Alejandro Campos Maestre
+ */
+public class NewClass
+{
+
+    public static void main(String[] args)
+    {
+        System.out.printf("%-15s%-30s%-15s%-20s%-21s%-15s%-27s%-1s\n", "Id_usuario", "Correo", "Nombre", "Primer apellido", 
+                "Segundo apellido", "Telefono", "Telefono de emergencia", "NickName");
+        
+        System.out.printf("%-15s%-30s%-15s%-20s%-21s%-15s%-27s%-1s\n", "1", "alex_-campos@hotmail.com", "Alejandro", "Campos", 
+                "Maestre", "456123456", "545632145", "Alex69");
+    }
+}

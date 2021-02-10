997c31007192d97d46120533c791e4464f0b95a4
AlexCampos98 <72565364+AlexCampos98@users.noreply.github.com>
2021-02-10 15:46:42 +0100

    Añadido de nuevo

--- bd/BorradoBD.sql
+++ bd/BorradoBD.sql
@@ -1,9 +1,12 @@
-DROP TABLE entrenamiento;
-DROP TABLE usuario;
 drop procedure MODIFICAR_ENTRENAMIENTO;
 drop procedure Modificar_usuario;
+
 drop trigger AUDITORIA_ENTRENO_DEPORTISTA;
 drop trigger AUDITORIA_MAX_ENTRENO;
 drop trigger NOM_APE_VOCALES;
+
 drop sequence SEQUENCE_ENTRENAMIENTO;
 drop sequence SEQUENCE_USUARIO;
+
+DROP TABLE entrenamiento;
+DROP TABLE usuario;
